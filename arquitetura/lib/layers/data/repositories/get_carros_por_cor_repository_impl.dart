c645c584769c5c7302c524d2352b81f06e892618
dandy <danielsbarbosa27@gmail.com>
2021-12-04 18:38:05 -0300

    ajustes

--- arquitetura/lib/layers/data/repositories/get_carros_por_cor_repository_impl.dart
+++ arquitetura/lib/layers/data/repositories/get_carros_por_cor_repository_impl.dart
@@ -1,5 +1,4 @@
 import 'package:arquitetura/layers/data/datasources/get_carros_por_cor_datasource.dart';
-import 'package:arquitetura/layers/data/dto/carro_dto.dart';
 import 'package:arquitetura/layers/domain/entities/carro_entity.dart';
 import 'package:arquitetura/layers/domain/repositories/get_carros_por_cor_repository.dart';
 
